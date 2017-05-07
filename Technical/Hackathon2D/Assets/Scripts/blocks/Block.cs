864ef9734e2ccf963e94495f53af4793b3b50555
Khang Dinh Hoang <taigama@users.noreply.github.com>
2017-05-07 14:44:57 +0700

    silce sprite, base block

---
+++ Technical/Hackathon2D/Assets/Scripts/blocks/Block.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType
+{
+    Dirt,
+    None,
+    Ground,
+    Iron,
+    Bronze,
+    Silver,
+    Gold,
+    GemBlue,
+    GemRed,
+    GemGreen,
+    GemWhite,
+    Bone,
+    BlockBlack,
+    BlockWhite,
+    Ground2
+}
+
+public class Block : MonoBehaviour
+{
+    
+
+    public BlockType type;
+
+    public int health;
+    
+    public void Awake()
+    {
+
+    }
+
+    public void OnHit()
+    {
+        
+
+
+        gameObject.SetActive(false);
+
+        // give something for player
+    }
+
+    public Vector2 Pos()
+    {
+        return new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+    }
+}
